22df53494dda0d61a2a6c58728f5ca6ffd8a1bec
Daniel <ddvrns@gmail.com>
2017-05-11 18:11:56 +0800

    database

---
+++ sadu/Models/User.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace sadu.Models
+{
+    public class User
+    {
+        public int ID { get; set;}
+        public String username { get; set; }
+        private String password { get; set; }
+        private bool isAdmin { get; set; }
+    }
+
+    public class UserDBContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+    }
+}
