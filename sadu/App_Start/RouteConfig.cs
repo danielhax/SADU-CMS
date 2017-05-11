02b5a3e479d4ca44c7fa3b80a057d2f135792d73
pldtautopilot1 <pldtautopilot1@gmail.com>
2017-05-11 13:17:07 +0800

    first commit

--- sadu/App_Start/RouteConfig.cs
+++ sadu/App_Start/RouteConfig.cs
@@ -14,9 +14,9 @@ namespace sadu
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
-                name: "Default",
+                name: "Login",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Login", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
